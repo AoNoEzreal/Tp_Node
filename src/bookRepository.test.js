9f2042458309a4b28b2fd8c6c70fadd7ba91ee48
AoNoEzreal <ybettayeb@gmail.com>
2019-10-09 19:24:05 +0200

    Erreur de nom

--- src/bookRepository.test.js
+++ src/bookRepository.test.js
@@ -1,4 +1,4 @@
-const BookRepository = require('./book.repository');
+const BookRepository = require('./bookRepository');
 
 describe('save method', () => {
     test('should run the db write method once', () => {
